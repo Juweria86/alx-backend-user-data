a670d49f0eff58a93e7c3a3c8146a863a48a244c
Juweria86 <jujumelmi143@gmail.com>
2024-08-03 19:30:52 +0300

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""""module doc"""
+import re
+from typing import List
+
+
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """"returns the log message obfuscated"""
+    pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
+    return re.sub(pattern, redaction, message)
