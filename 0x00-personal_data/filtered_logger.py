2798646c8fa9ffef9d420d114030918fca4e9c79
Juweria86 <jujumelmi143@gmail.com>
2024-08-03 20:37:25 +0300

    Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """"module doc"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -10,3 +11,24 @@ def filter_datum(
     """"returns the log message obfuscated"""
     pattern = '|'.join([f'(?<={field}=)[^{separator}]+' for field in fields])
     return re.sub(pattern, redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """doc doc doc"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """doc doc doc"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """func doc"""
+        org = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, org, self.SEPARATOR)
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
