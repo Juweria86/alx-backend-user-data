663f513325e7740ef8987aaa5fe812a75ab88b05
Juweria86 <jujumelmi143@gmail.com>
2024-08-09 06:03:16 +0300

    changed users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 """ Module of Users views
 """
-from flask import abort, jsonify, request
-
 from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
+from os import getenv
+
+
+if getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
@@ -28,23 +33,15 @@ def view_one_user(user_id: str = None) -> str:
     """
     if user_id is None:
         abort(404)
-    # If the user_id is "me" and there is no current_user, return a 404 error
+
     if user_id == 'me':
-        if request.current_user is None:
+        if not request.current_user:
             abort(404)
-        else:
-            # If the user_id is "me" and there is a current_user, return the
-            # JSON representation of the current_user
-            return jsonify(request.current_user.to_json())
-    # If user_id is None, return a 404 error
-    if user_id is None:
-        abort(404)
-    # Retrieve the user from the database using the User.get method
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
-    # If the user was not found, return a 404 error
     if user is None:
         abort(404)
-    # Return the JSON representation of the user
     return jsonify(user.to_json())
 
 
